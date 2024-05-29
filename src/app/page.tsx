120037c4fd12e99ae473d3e0dc24a1a1d465c1c6
MatheusPlinio <mplinio13@gmail.com>
2024-05-29 01:48:22 +0000

    fix redirect login auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import { getServerSession } from "next-auth";
 import FormSignIn from "./formSignIn";
+import { redirect } from "next/navigation";
+import { nextAuthOptions } from "@/lib/authOptions";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(nextAuthOptions)
+
+  if (session) {
+    redirect('/dashboard')
+  }
+
   return (
     <div className="flex justify-center items-center">
       <FormSignIn />
